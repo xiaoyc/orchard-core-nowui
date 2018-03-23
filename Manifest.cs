a65e171c881192fd40e7dcc0fc122f5d4832c239
Jasmin Savard <jasmin.savard@gmail.com>
2018-03-22 21:01:26 -0400

    adding files

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Now UI Theme",
+    Author = "The Orchard Team",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "Now UI Theme Kit."
+)]
